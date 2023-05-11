4b8fc6a6797f6544f64bd3305380661dfcb091b7
Felipe Pires <felipe.pires@weipa.com.br>
2023-05-11 15:18:53 -0300

    uploading minimal api.

---
+++ INOW.API/(Entities)/User.cs
@@ -0,0 +1,16 @@
+﻿namespace INOW.API._Entities_
+{
+    public partial class User : Base<int>
+    {
+        public string Name { get; set; }
+
+        public string FamilyName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Password { get; set; }
+    }
+
+}
